03c77e51c7017ba65bc308bf11c4a3b3631ac358
abbatistam <antoniobbatistam@gmail.com>
2022-10-15 20:45:33 -0400

    update main.go

---
+++ arraysAndSlices/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func isPalindrome(text string) {
+	var textReverse string
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+
+	}
+
+}
+
+func main() {
+
+	// Array
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
+
+	//Slices
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	// Metodos en el sclice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Apend
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	// Append nueva list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
+
+	stringSlice := []string{"hola", "que", "hace"}
+
+	for i, valor := range stringSlice {
+		fmt.Println(i, valor)
+	}
+
+}
